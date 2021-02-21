355242e29e7fbaef2c60d4bd510caf562ce873b3
jefersonmp <79395604+jefersonmp@users.noreply.github.com>
2021-02-21 04:39:33 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,56 @@
+const help = (prefixo) => {
+ return `
+ ╔══✪〘 INFO 〙✪══
+ ║
+ ╠➥ BOT INSANO
+ ╠➥ * 3,0 *
+ ╠➥ apk
+ ╠➥ * wa.me / + 5561998242169 *
+ ╠➥ Bot INSANO : LIGADO
+ ║
+ ╠══✪〘 MENU 〙✪══
+ ║
+ ╠➥ * adesivo . {prefix} *
+ ╠➥ * . {prefix} toimg *
+ ╠➥ * . {prefix} meme *
+ ╠➥ * . {prefix} memeindo *
+ ╠➥ * . {prefix} gtts *
+ ╠➥ * . {prefix} loli *
+ ╠➥ * . {prefix} nsfwloli *
+ ╠➥ * . {prefix} url2img *
+ ╠➥ * . {prefix} ocr *
+ ╠➥ * . {prefix} espere *
+ ╠➥ * . {prefix} setprefix *
+ ║
+ ╠══✪〘 OUTROS 〙✪══
+ ║
+ ╠➥ * . {prefix} grupo de links *
+ ╠➥ * . {prefix} simih [1/0] *
+ ╠➥ * . {prefix} marcar *
+ ╠➥ * . {prefix} adicione [@] *
+ ╠➥ * . {prefix} kick [@] *
+ ╠➥ *  . {prefix} promova [@] *
+ ╠➥ * . {prefix} rebaixar *
+ ╠➥ * . {prefix} listadmins *
+ ╠➥ * . {prefix} marcar2 *
+ ╠➥ * . {prefix} bc [texto] *
+ ╠➥ * . {prefix} marcar3 *
+ ╠➥ * . {prefix} lista de bloqueio *
+ ╠➥ * bloco . {prefix} [@] *
+ ╠➥ * . {prefix} desbloquear [@] *
+ ╠➥ * . {prefix} clearall *
+ ╠➥ * . {prefix} bc [* texto *] *
+ ╠➥ * . {prefix} bem-vindo [1/0] *
+ ╠➥ * . {prefix} clone [@] *
+ ╠➥ * . {prefix} Insanomenu *
+ ╠➥ * . {prefix} dono *
+ ║
+ ╠══✪〘 BASE 〙✪══
+ ║
+ ╠➥ INSANO OFC
+ ╠➥ primeiro de muitos ai
+ ║
+ ╚═〘 CASAL INSANO 〙`
+ }
+
+ exportações.help = ajuda
---
+++ src/help1.js
@@ -0,0 +1,34 @@
+const help1 = (prefix) => {
+
+	return `
+╔══✪〘 INFO 〙✪══
+║
+╠➥ 
+╠➥ *3.0*
+╠➥   ☔
+╠➥ *wa.me/+5561998242169*
+╠➥ Bot INSANO: ON
+║
+╠══✪〘 MENU1 〙✪══
+║
+╠➥ *${prefix}dono*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+╠➥ *${prefix}*
+║
+╠══✪〘 *FIM* 〙✪══
+
+════════════════════
+*Insano * 🤗
+*Digite .{prefix}dono para mais info*
+════════════════════`
+}
+exports.help1 = help1
+
